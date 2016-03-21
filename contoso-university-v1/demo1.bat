581cbc10d7ffd9d576ff4a8960c26f17d31cdbe6
jaimegag <jaguilar@pivotal.io>
2016-03-21 11:58:41 -0400

    demo.bat changes

--- contoso-university-v1/demo1.bat
+++ contoso-university-v1/demo1.bat
@@ -1,7 +1,10 @@
-cf push contoso-demo-v1 -b https://github.com/ryandotsmith/null-buildpack.git -s windows2012R2 --no-start
-cf enable-diego contoso-demo-v1
+cf push contoso-demo-v1 -b binary_buildpack  -s windows2012R2 --no-start
 cf set-health-check contoso-demo-v1 none
 cf bind-service contoso-demo-v1 contoso-demo-sql
 cf bind-service contoso-demo-v1 contoso-demo-queue
 cf start contoso-demo-v1
-cf map-route contoso-demo-v1 YOUR_DOMAIN -n contoso
+cf map-route contoso-demo-v1 apps.10.65.226.17.piv.io -n contoso
+
+REM Data Source=jagappsdb.database.windows.net;Integrated Security=False;User ID=jaguilar@jagappsdb;Password=Jg1g53l1r@;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Initial Catalog=Contoso_DB
+
+REM Endpoint=sb://jagapps.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=YxsEg9cI02BJPognvmHTSKPhfuU/MdFzZfCGCtsI4ww=
